880cf5d4d3baf92c29722a189e49fdd36614e77c
Johannes Cierpinski <jcierpinski@ai-ag.de>
2025-07-01 17:39:58 +0200

    Switch to Gradle

--- src/test/java/com/jcs3/hbt/HbtApplicationTests.java
+++ src/test/java/com/jcs3/hbt/HbtApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HbtApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+	@Test
+	void contextLoads() {
+	}
 
 }
